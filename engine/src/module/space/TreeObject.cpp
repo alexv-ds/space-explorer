03226dd2462d5fb11d57b3a4f0a7f7247fdcf1bf
sasichkamega <sasichkamega@gmail.com>
2022-08-01 16:06:29 +0300

    implement tree object

---
+++ engine/src/module/space/TreeObject.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <cassert>
+#include "TreeObject.hpp"
+#include "SpatialIndex.hpp"
+
+namespace engine::space::internal {
+
+TreeObject::TreeObject() = default;
+TreeObject::~TreeObject() {
+  assert(this->p_index == nullptr);
+};
+
+void TreeObject::index_lock(std::shared_ptr<SpatialIndex> p_index) noexcept {
+  this->p_index = std::move(p_index);
+}
+void TreeObject::index_unlock() noexcept {
+  this->p_index = nullptr;
+}
+
+void TreeObject::update() {
+  assert(this->p_index != nullptr);
+  update_bbox();
+  this->p_index->update_object(this);
+}
+
+}
--- engine/src/module/space/TreeObject.hpp
+++ engine/src/module/space/TreeObject.hpp
@@ -7,26 +7,32 @@
 
 namespace engine::space::internal {
 
-class TreeObject final : public std::enable_shared_from_this<TreeObject>, public utils::noncopyable {
+class TreeObject final : public utils::noncopyable {
 public:
   glm::vec2 position; //object center
   glm::vec2 size;
   flecs::entity_t holder;
 
-  ~TreeObject();
+  void update();
 private:
   friend class BBoxExtractor;
   friend class SpatialIndex;
+  friend class TreeObjectDeleter;
 
   TreeObject();
+  ~TreeObject();
+
   inline void update_bbox() noexcept;
+  void index_lock(std::shared_ptr<SpatialIndex>) noexcept;
+  void index_unlock() noexcept;
 
   float left;
   float top;
   float width;
   float height;
-};
 
+  std::shared_ptr<SpatialIndex> p_index;
+};
 
 inline void TreeObject::update_bbox() noexcept {
   left = position.x - size.x / 2.0f;
@@ -40,8 +46,5 @@ inline void TreeObject::update_bbox() noexcept {
     (*out) = in->bbox;
   }
 };*/
-
-
-
   
 }
