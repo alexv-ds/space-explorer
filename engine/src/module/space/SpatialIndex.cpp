41bb6904f629dc05fb365c1596ef62d6bea46fce
sasichkamega <sasichkamega@gmail.com>
2022-08-01 16:21:55 +0300

    index

--- engine/src/module/space/SpatialIndex.cpp
+++ engine/src/module/space/SpatialIndex.cpp
@@ -6,6 +6,10 @@ std::shared_ptr<SpatialIndex> SpatialIndex::create_index() {
   return std::shared_ptr<SpatialIndex>(new SpatialIndex);
 }
 
-
+SpatialIndex::object_ptr SpatialIndex::create_object() {
+  TreeObject* p_object = new TreeObject();
+  //p_object->index_lock(shared_from_this());
+  //return SpatialIndex::object_ptr(p_object, TreeObjectDeleter());
+}
 
 }
--- engine/src/module/space/SpatialIndex.hpp
+++ engine/src/module/space/SpatialIndex.hpp
@@ -6,25 +6,26 @@
 
 namespace engine::space::internal {
 
+template<class T> class TreeObjectDeleter;
+
 class SpatialIndex final : public std::enable_shared_from_this<SpatialIndex>, public utils::noncopyable {
 public:
   using object_ptr = std::shared_ptr<TreeObject>;
-
   static std::shared_ptr<SpatialIndex> create_index();
 
-
-  //not thread safe
-  void update_object(TreeObject*);
-  //thread safe functions
   object_ptr create_object();
 private:
-  void destroy_object(TreeObject*);
+  friend class TreeObject;
+  friend template<class T> class TreeObjectDeleter;
+
+  void destroy_object(TreeObject*) {}
+  void update_object(TreeObject*) {}
   
   moodycamel::ConcurrentQueue<object_ptr> insert_queue;
   moodycamel::ConcurrentQueue<object_ptr> destroy_queue;
 
-  SpatialIndex() = default;
-
+  SpatialIndex();
+  SpatialIndex();
 };
 
 
